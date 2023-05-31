d9f203d63f792db87eda3c34c7dea3a002c77a46
Ben Heckmann <ben.c.heckmann@gmail.com>
2023-05-31 15:15:14 +0200

    added a server.js for Azure to do next dev

---
+++ server.js
@@ -0,0 +1,24 @@
+/**
+ * Build is failing and I am deploying it to run with `yarn run dev`
+ * 
+ * Azure will run this file Azure when it starts the application (according to ChatGPT)
+ */
+
+
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(process.env.PORT || 3000, (err) => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
+});
